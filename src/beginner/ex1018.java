c42a41454295c960fb68500da4d8de0a8b2b3dee
cristovanlopes <cristovanl1@gmail.com>
2022-05-23 20:46:55 -0300

    zxz\x

--- src/beginner/ex1018.java
+++ src/beginner/ex1018.java
@@ -3,14 +3,14 @@ import java.io.IOException;
 import java.util.Scanner;
 
 public class ex1018 {
- 
+
     public static void main(String[] args) throws IOException {
  Scanner sc = new Scanner(System.in);
 		int n, cem, cinq, vint, dez, cinco, dois, um, n1;
 	    n = sc.nextInt();
 	    n1=n;
-	    cem=n/100;n=n-cem*100;    
-	    cinq=n/50;n=n-cinq*50;    
+	    cem=n/100;n=n-cem*100;
+	    cinq=n/50;n=n-cinq*50;
 	    vint=n/20; n=n-vint*20;
 	    dez=n/10; n=n-dez*10;
 	    cinco=n/5;n=n-cinco*5;
@@ -24,8 +24,8 @@ public class ex1018 {
 	    System.out.println(cinco + " nota(s) de R$ 5,00");
 	    System.out.println(dois + " nota(s) de R$ 2,00");
 	    System.out.println(um + " nota(s) de R$ 1,00");
-	    
+
 	    sc.close();
     }
- 
+
 }
