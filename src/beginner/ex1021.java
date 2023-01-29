66cfe7fa4069fec3ec7ec84721fd6fefa1a6b161
Cristovan Lopes Araujo dos Santos <50528087+cristovanlopes@users.noreply.github.com>
2023-01-28 22:13:36 -0300

    Update ex1021.java

--- src/beginner/ex1021.java
+++ src/beginner/ex1021.java
@@ -1,48 +1,35 @@
 package beginner;
-
-import java.util.Scanner;
+import java.util.Locale;
 
 public class ex1021 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		//noteBanks
-		int hundredNoteBank, fiftyNoteBank, twentyNoteBank, tenNoteBank, fiveNoteBank, twoNoteBank, oneNoteBank;
-		//Coins
-		int fiftyCoin, twentyFiveCoin, tenCoin, fiveCoin, oneCentCoin;
-		double n = sc.nextDouble();
-		double valor_decimal = n - (int)(n);
-		valor_decimal = valor_decimal *100;
-		int noteBank = (int)n;
-		int coins = (int)valor_decimal;
-		
+		Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        double n = sc.nextDouble();
+        int[] notes = { 100, 50, 20, 10, 5, 2 };
+        int[] coins = { 50, 25, 10, 5, 1 };
+        String[] noteNames = { "nota(s) de R$ 100.00", "nota(s) de R$ 50.00", "nota(s) de R$ 20.00", "nota(s) de R$ 10.00", "nota(s) de R$ 5.00", "nota(s) de R$ 2.00" };
+        String[] coinNames = { "moeda(s) de R$ 0.50", "moeda(s) de R$ 0.25", "moeda(s) de R$ 0.10", "moeda(s) de R$ 0.05", "moeda(s) de R$ 0.01" };
 
-	    hundredNoteBank=noteBank/100;noteBank=noteBank-hundredNoteBank*100;    
-	    fiftyNoteBank=noteBank/50;noteBank=noteBank-fiftyNoteBank*50;    
-	    twentyNoteBank=noteBank/20; noteBank=noteBank-twentyNoteBank*20;
-	    tenNoteBank=noteBank/10; noteBank=noteBank-tenNoteBank*10;
-	    fiveNoteBank=noteBank/5;noteBank=noteBank-fiveNoteBank*5;
-	    twoNoteBank=noteBank/2;noteBank=noteBank-twoNoteBank*2;
-	    oneNoteBank=noteBank/1;
+        int noteCount, coinCount;
+        double coinValue = n - (int)n;
+        coinValue *= 100;
 
-		System.out.println("NOTAS:");
-	    System.out.println(hundredNoteBank + " nota(s) de R$ 100,00");
-	    System.out.println(fiftyNoteBank + " nota(s) de R$ 50,00");
-	    System.out.println(twentyNoteBank + " nota(s) de R$ 20,00");
-	    System.out.println(tenNoteBank + " nota(s) de R$ 10,00");
-	    System.out.println(fiveNoteBank + " nota(s) de R$ 5,00");
-	    System.out.println(twoNoteBank + " nota(s) de R$ 2,00");
-	    
-	    System.out.println("MOEDAS");
-	    System.out.println(oneNoteBank + " moeda(s) de R$ 1,00");
-		
-	    System.out.println(fiftyCoin + " moeda(s) de R$ 0,50");
-	    System.out.println(twentyFiveCoin + " moeda(s) de R$ 0,25");
-	    System.out.println(tenCoin + " moeda(s) de R$ 0,10");
-	    System.out.println(fiveCoin + " moeda(s) de R$ 0,05");
-	    System.out.println(oneCentCoin + " moeda(s) de R$ 0,01");
-		
-		System.out.println("Teste " + coins);
+        System.out.println("NOTAS:");
+        for (int i = 0; i < notes.length; i++) {
+            noteCount = (int)n / notes[i];
+            n -= noteCount * notes[i];
+            System.out.println(noteCount + " " + noteNames[i]);
+        }
+
+        System.out.println("MOEDAS:");
+        System.out.println((int)n + " moeda(s) de R$ 1.00");
+        for (int i = 0; i < coins.length; i++) {
+            coinCount = (int)coinValue / coins[i];
+            coinValue -= coinCount * coins[i];
+            System.out.println(coinCount + " " + coinNames[i]);
+		}
+        
 		sc.close();
 	}
 
